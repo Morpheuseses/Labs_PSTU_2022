bdd86fe21651481d94a9248e3f89fb164bf12c95
Matvey <115172758+Morpheuseses@users.noreply.github.com>
2023-03-05 18:04:19 +0500

    Update bucketSort.cpp

--- semester_2/massives/advance_sort/bucketSort/bucketSort.cpp
+++ semester_2/massives/advance_sort/bucketSort/bucketSort.cpp
@@ -7,7 +7,7 @@ void fullArray(int* arr, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
-		arr[i] = rand() % 10;
+		arr[i] = rand() % 20;
 	}
 }
 void showArray(int* arr, int size)
@@ -24,41 +24,45 @@ void swap(int& a, int& b)
 	a = b;
 	b = tmp;
 }
-void bucketSort(int* arr, int size)
+void bucketSort(int* arr)
 {
-	int cnt_b = 2; // >10, <10 - 2 buckets 
-	int** bucket = new int*[cnt_b];
-	for (int i = 0; i < size; i++)
+	const int SIZE_ARRAY = 10;
+	const int CNT_B = 2;
+
+	int buckets[CNT_B][SIZE_ARRAY];
+
+	int bucketSizes[CNT_B]{ 0 };
+
+	for (int i = 0; i < SIZE_ARRAY; i++) 
 	{
-		bucket[i] = new int[cnt_b];
-	}
-	int* sizes = new int[cnt_b];
-	for (int i = 0; i < cnt_b; i++)
-	{
-		sizes[i] = 0;
-	}
-	for (int i = 0; i < size; i++)
-	{
-		int bucketindex = arr[i] / cnt_b;
-		bucket[bucketindex][sizes[bucketindex]] = arr[i];
-		sizes[bucketindex]++;
-	}
-	for (int i = 0; i < cnt_b; i++)
-	{
-		for (int j = 0; j < sizes[i]; j++)
+		if (arr[i] > 10)  
 		{
-			for (int k = j; k > 0 && bucket[j][k-1] > bucket[j][k]; k--)
+			buckets[1][bucketSizes[1]] = arr[i];
+			bucketSizes[1]++;
+		}
+		if (arr[i] < 10)
+		{
+			buckets[0][bucketSizes[0]] = arr[i];
+			bucketSizes[0]++;
+		}
+	}
+	for (int i = 0; i < CNT_B; i++) {    // it finally worked
+		for (int j = 0; j < bucketSizes[i]; j++)
+		{
+			for (int k = j; k > 0 && buckets[i][k - 1] > buckets[i][k]; k--) // important that it must be bucket[i]
 			{
-				swap(bucket[j][k - 1], bucket[j][k]);
+				swap(buckets[i][k - 1], buckets[i][k]); // and here too
 			}
 		}
-	}
-	int index = 0;
-	for (int i =0; i < cnt_b; i++)
-		for (int j = 0; j < sizes[i]; j++)
+	}													// I wasted so much on this. I hate debugging code 
+	int idx = 0;
+	for (int i = 0; i < CNT_B; i++)
+	{
+		for (int j = 0; j < bucketSizes[i]; j++)
 		{
-			arr[index++] = bucket[i][j];
+			arr[idx++] = buckets[i][j];
 		}
+	}
 }
 
 
@@ -69,7 +73,7 @@ int main()
 	int* a = new int[size];
 	fullArray(a, size);
 	showArray(a, size);
-	bucketSort(a, size);
+	bucketSort(a);
 	showArray(a, size);
 	return 0;
 }
