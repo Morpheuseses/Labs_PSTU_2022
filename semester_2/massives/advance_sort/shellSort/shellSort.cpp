facaa12f2ef5cbe25ba8b7ca7c0981a7f96dace3
Matvey <115172758+Morpheuseses@users.noreply.github.com>
2023-03-06 13:37:48 +0500

    lists have completed,  stack and queue will come

---
+++ semester_2/massives/advance_sort/shellSort/shellSort.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std; 
+
+void fillArray(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 20;
+	}
+}
+void showArray(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl << endl;
+}
+void shellSort(int* arr, int n)
+{
+	for (int gap = n / 2; gap > 0; gap /= 2)
+	{
+
+
+		for (int i = gap; i < n; i++)
+		{
+
+			int tmp = arr[i];
+
+			int j;
+			for (j = i; j >= gap && arr[j - gap] > tmp; j -= gap)
+			{
+				arr[j] = arr[j - gap];
+			}
+
+			arr[j] = tmp;
+
+		}
+	}
+}
+
+int main()
+{
+	srand(time(NULL));
+	int size = 10;
+	int* a = new int[size];
+	fillArray(a,size);
+	showArray(a, size);
+	shellSort(a, size);
+	showArray(a, size);
+	return 0; 
+}
