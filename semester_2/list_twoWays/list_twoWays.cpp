232c6c103a8ccbb829341a3a7f0d01381edb48d6
Matvey <115172758+Morpheuseses@users.noreply.github.com>
2023-03-13 13:27:23 +0500

    list_twoWays update

--- semester_2/list_twoWays/list_twoWays.cpp
+++ semester_2/list_twoWays/list_twoWays.cpp
@@ -1,139 +1,219 @@
 #include<iostream>
-using namespace std; 
-
 
 struct ListElem
 {
+	int data;
 	ListElem* next;
 	ListElem* prev;
-	int value;
 };
 
 ListElem* createList(int n)
 {
-	ListElem* start;
-	ListElem* p, *r;
-	start = new ListElem;
-	cin >> start->value;
-	start->next = nullptr;
+	ListElem* start = new ListElem;
+	ListElem* curr, * prev;
 	start->prev = nullptr;
-	p = start;
+	std::cout << "Enter a data of an element: ";
+	std::cin >> start->data;
+	prev = start;
 	for (int i = 0; i < n - 1; i++)
 	{
-		r = new ListElem;
-		cin >> r->value;
-		r->next = nullptr;
-		r->prev = p;
-		p->next = r;
-		p = r;
+		ListElem* curr = new ListElem;
+		std::cout << "Enter a data of an element: ";
+		std::cin >> curr->data;
+		curr->next = nullptr;
+		curr->prev = prev;
+		prev->next = curr;
+		prev = curr;
 	}
 	return start;
 }
-void addElem(ListElem* list, int elem, int pos, int& size)
+
+void addElem(ListElem*& list, int pos, int data, int& size)
 {
-	size++;
-	ListElem* px = list, *pend;
-	for (int i = 1; i < pos - 1; i++)
-	{
-		px = px->next;
-	}
-	pend = px->next;
-	ListElem* tmp = new ListElem;
 	if (pos == 1)
 	{
-		tmp->prev = nullptr;
-		tmp->value = elem;
+		ListElem* start = new ListElem;
+		start->data = data;
+		start->prev = nullptr;
+		start->next = list;
+		list->prev = start;
+		list = start; // if it will work, it will be cool
 	}
-	else
+	/*if (pos == size)
 	{
-		tmp->prev = px;
-		px->next = tmp;
-	}
-	tmp->next = pend;
-	
-}
-void deleteElem(ListElem* list, int pos, int& size)
-{
-	size--;
-	ListElem* px = list, *pend;
-	for (int i = 1; i < pos - 1; i++)
+		ListElem* curr = list;
+		for (int i = 1; i < size; i++)
+		{
+			curr = curr->next;
+		}
+		ListElem* end = new ListElem;
+		end->data = data;
+		end->next = nullptr;
+		end->prev = curr;
+		curr->next = end;
+	}*/
+	if (pos != 1)
 	{
-		px = px->next;
+		ListElem* elem = new ListElem;
+		elem->data = data;
+		ListElem* curr = list;
+		for (int i = 1; i < pos - 1; i++)
+			curr = curr->next;
+		
+		elem->prev = curr;
+		elem->next = curr->next;
+		curr->next = elem;
+		
 	}
-	pend = px->next->next;
-	px->next = pend;
+	size++;
 }
-void addElems(ListElem* list, int pos, int& size)
+void addElems(ListElem*& list, int pos, int& size, int cnt)
 {
-	cout << "Enter count of elements you want to add:";
-	int cnt;
-	cin >> cnt;
+
+	ListElem* curr, * prev, * start = new ListElem;
+	std::cout << "Enter data of the next element: ";
+	std::cin >> start->data;
+	start->prev = nullptr;
+	prev = start;
+	for (int i = 1; i < cnt; i++)
+	{
+		curr = new ListElem;
+		std::cout << "Enter a data of the next element: ";
+		std::cin >> curr->data;
+		curr->next = nullptr;
+		curr->prev = prev;
+		prev->next = curr;
+		prev = curr;
+	}
+	if (pos == 1)
+	{
+		list->prev = prev;
+		list = start;
+	}
+	/*if (pos == size)
+	{
+		ListElem* curr = list;
+		for (int i = 1; i <= size; i++)
+		{
+			curr = curr->next;
+		}
+		curr->next = start;
+		start->prev = curr;
+		prev->next = nullptr;
+	}*/
+	if (pos != 1 && pos != size)
+	{
+		ListElem* curr = list;
+		for (int i = 1; i < pos - 1; i++)
+		{
+			curr = curr->next;
+		}
+		prev->next = curr->next;
+		curr->next->prev = prev;
+		curr->next = start;
+	}
 	size += cnt;
-	ListElem* px = list;
-	for (int i = 1; i < pos - 1; i++)
-	{
-		px = px->next;
-	}
-	ListElem* next = px->next;
-	ListElem* prev = px;
-	ListElem* tmp = new ListElem;
-	for (int i = 0; i < cnt; i++)
-	{
-		cout << "Enter next element: ";
-		int value;
-		tmp = new ListElem;
-		cin >> value;
-		tmp->value = value;
-		tmp->prev = prev;
-		prev->next = tmp;
-		prev = tmp;
-	}
-	tmp->next = next;
 }
-void deleteElems(ListElem* list, int pos, int& size)
+void removElem(ListElem*& list, int pos, int& size)
 {
-	cout << "Enter count of elements you want to delete:";
-	int cnt;
-	cin >> cnt;
-	size -= cnt;
-	ListElem* px = list;
-	for (int i = 1; i < pos - 1; i++)
+	if (pos == 1)
 	{
-		px = px->next;
+		list = list->next;
 	}
-	ListElem* pend = px;
-	for (int i = 1; i <= cnt; i++)
+	if (pos == size)
 	{
-		pend =pend->next;
+		ListElem* curr = list;
+		for (int i = 1; i < size; i++)
+		{
+			curr = curr->next;
+		}
+		curr->next = nullptr;
 	}
-	px->next = pend->next;
-	pend->prev = px;
+	if (pos != size && pos != 1)
+	{
+		ListElem* curr = list;
+		for (int i = 1; i < pos - 1; i++)
+		{
+			curr = curr->next;
+		}
+		curr->next = curr->next->next;
+		curr->next->prev = curr;
+	}
+	size--;
+}
+void removElems(ListElem*& list, int pos, int& size, int cnt)
+{
+	if (cnt > size)
+	{
+		list->next = nullptr;
+	}
+	if (pos == 1)
+	{
+		ListElem* curr = list;
+		for (int i = 1; i <= cnt; i++)
+		{
+			curr = curr->next;
+		}
+		curr->prev = nullptr;
+		list = curr;
+	}
+	if (pos != 1)
+	{
+		ListElem* curr = list, * pend; // pend - pointer to "tail" of list
+		for (int i = 1; i < pos - 1; i++)
+		{
+			curr = curr->next;
+		}
+		pend = curr;
+		for (int i = 1; i <= cnt; i++)
+		{
+			pend = pend->next;
+		}
+		curr->next = pend->next;
+		pend->prev = curr;
+	}
+	size-=cnt;
+}
+void showList(ListElem* list, int size)
+{
+	std::cout << "Here your list" << std::endl;
+	std::cout << "Begin <-> ";
+	ListElem* curr = list;
+	std::cout << curr->data << " <-> ";
+	for (int i = 1; i < size; i++)
+	{
+		curr = curr->next;
+		std::cout << curr->data << " <-> ";
+	}
+	std::cout << "End" << std::endl;
+
 }
 int main()
 {
-	int size = 5;
-	cout << "Enter list elements values: " << endl;
-	ListElem* list = createList(size);
-	addElem(list, 1, 3, size);
-	deleteElem(list, 4, size);
-	addElems(list, 2, size);
-	ListElem* tmp = list;
-	for (int i = 1; i <= size; i++)
-	{
-		cout << tmp->value << " ";
-		if (tmp->next!=nullptr)
-			tmp = tmp->next;
-	}
-	cout << endl;
-	deleteElems(list, 2, size);
-	tmp = list;
-	for (int i = 1; i <= size; i++)
-	{
-		cout << tmp->value << " ";
-		if (tmp->next != nullptr)
-			tmp = tmp->next;
-	}
-	cout << endl;
-	delete[] list;
+	int size = 5; // quantity of elements in the list, we will create
+	ListElem* list = new ListElem;
+	list = createList(size);
+	showList(list, size);
+	int pos;
+	std::cout << "Enter a position of an element you want to add: ";
+	std::cin >> pos;
+	int data;
+	std::cout << "Enter data of the element, you add: ";
+	std::cin >> data;
+	addElem(list, pos, data, size);
+	showList(list, size);
+	int cnt;
+	std::cout << "Enter pos of the element from which you want to add, and count of this elements: ";
+	std::cin >> pos >> cnt;
+	addElems(list, pos, size, cnt);
+	showList(list, size);
+	std::cout << "Enter position of the element, which will bw deleted: ";
+	std::cin >> pos;
+	removElem(list, pos, size);
+	showList(list, size);
+	std::cout << "Enter position of an element from which element will be deleted and count of them: ";
+	std::cin >> pos >> cnt;
+	removElems(list, pos, size, cnt);
+	showList(list, size);
 	return 0;
 }
