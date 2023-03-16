be833d4b8008bd64469fced0c5404f6549fe9946
Matvey <115172758+Morpheuseses@users.noreply.github.com>
2023-03-16 20:51:34 +0500

    update lab 11

---
+++ semester_2/queue/queue.cpp
@@ -0,0 +1,204 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+	int data;
+	Node* next;
+};
+struct Queue
+{
+	Node* front;
+	Node* back;
+	int size;
+	void init()
+	{
+		size = 0;
+		front = nullptr;
+		back = nullptr;
+	}
+};
+void Push(Queue*& queue, int data)
+{
+	Node* tmp = new Node;
+	tmp->data = data;
+	tmp->next = nullptr;
+	if (queue->size == 0)
+	{
+		queue->front = tmp;
+		queue->back = tmp;
+	}
+	else
+	{
+		queue->back->next = tmp;
+		queue->back = tmp;
+	}
+	queue->size++;
+}
+void Pop(Queue*& queue)
+{
+	if (queue->size == 0) 
+	{
+		return;
+	}
+	queue->front = queue->front->next;
+	queue->size--;
+}
+
+void insertElem(Queue*& queue, int pos, int data)
+{
+	Queue* tmp = new Queue;
+	tmp->init();
+	for (int i = 1; i < pos; i++)
+	{
+		Push(tmp, queue->front->data);
+		Pop(queue);
+	}
+	Push(tmp, data);
+	int init_size = queue->size;
+	for (int i = 1; i <= init_size; i++)
+	{
+		Push(tmp, queue->front->data);
+		Pop(queue);
+	}
+	queue = tmp;
+}
+void insertElems(Queue*& queue, int pos, int cnt)
+{
+	Queue* tmp = new Queue;
+	tmp->init();
+	for (int i = 1; i < pos; i++)
+	{
+		Push(tmp, queue->front->data);
+		Pop(queue);
+	}
+	int data;
+	for (int i = 1; i <= cnt; i++)
+	{
+		cout << "Enter data of next element: ";
+		cin >> data;
+		Push(tmp, data);
+	}
+	int init_size = queue->size;
+	for (int i = 1; i <= init_size; i++)
+	{
+		Push(tmp, queue->front->data);
+		Pop(queue);
+	}
+	queue = tmp;
+}
+void removElem(Queue*& queue, int pos)
+{
+	Queue* tmp = new Queue;
+	tmp->init();
+	for (int i = 1; i < pos; i++)
+	{
+		Push(tmp, queue->front->data);
+		Pop(queue);
+	}
+	Pop(queue);
+	int init_size = queue->size;
+	for (int i = 1; i <= init_size; i++)
+	{
+		Push(tmp, queue->front->data);
+		Pop(queue);
+	}
+	queue = tmp;
+}
+void removElems(Queue*& queue, int pos, int cnt)
+{
+	Queue* tmp = new Queue;
+	tmp->init();
+	for (int i = 1; i < pos; i++)
+	{
+		Push(tmp, queue->front->data);
+		Pop(queue);
+	}
+	for (int i = 1; i <= cnt; i++)
+	{
+		Pop(queue);
+	}
+	int init_size = queue->size;
+	for (int i = 1; i <= init_size; i++)
+	{
+		Push(tmp, queue->front->data);
+		Pop(queue);
+	}
+	queue = tmp;
+}
+void showQueue(Queue*& queue)
+{
+	Node* curr = new Node;
+	curr = queue->front;
+	cout << "Begin -> ";
+	for (int i = 1; i <= queue->size; i++)
+	{
+		cout << curr->data << " -> ";
+		curr = curr->next;
+	}
+	cout << "End" << endl;
+}
+int main()
+{
+	Queue* queue = new Queue;
+	queue->init();
+	int data, pos, cnt, choice;
+	for (int i = 1; i < 5; i++)
+	{
+		cout << "Enter data of an element: ";
+		cin >> data;
+		Push(queue, data);
+	}
+	showQueue(queue);
+	cout << "Choose the optiion: " << endl;
+	cout << "1 - Push, 2 - Pop, 3 - insertElem, 4 - InsertElems, 5 - remove Elem, 6 - remove Elems " << endl;
+	cin >> choice;
+	switch (choice)
+	{
+		case 1:
+		{
+			cout << "Enter data of element, you want to add: ";
+			cin >> data;
+			Push(queue, data);
+			showQueue(queue);
+			break;
+		}
+		case 2:
+		{
+			Pop(queue);
+			showQueue(queue);
+			break;
+		}
+		case 3:
+		{
+			cout << "Enter position and data of an element, you want to insert: ";
+			cin >> pos >> data;
+			insertElem(queue, pos, data);
+			showQueue(queue);
+			break;
+		}
+		case 4:
+		{
+			cout << "Enter position and quantity, you want to insert: ";
+			cin >> pos >> cnt;
+			insertElems(queue, pos, cnt);
+			break;
+		}
+		case 5:
+		{
+			cout << "Enter position of element, which will be removed";
+			cin >> pos;
+			removElem(queue, pos);
+			showQueue(queue);
+			break;
+		}
+		case 6:
+		{
+			cout << "Enter position and quantity of element, which will be removed: ";
+			cin >> pos >> cnt;
+			removElems(queue, pos, cnt);
+			showQueue(queue);
+			break;
+		}
+	}
+}
---
+++ semester_2/stack/stack.cpp
@@ -0,0 +1,225 @@
+#include<iostream>
+using namespace std;
+
+
+struct Node
+{
+	int data;
+	Node* next;
+};
+struct Stack
+{
+	Node* top;
+	int size;
+	void init()
+	{
+		top = nullptr;
+		size = 0;
+	}
+};
+void Push(Stack*& stack, int data)
+{
+	Node* tmp = new Node;
+	tmp->data = data;
+	tmp->next = stack->top;
+	stack->top = tmp;
+	stack->size++;
+}
+void Pop(Stack*& stack)
+{
+	if (stack->top == nullptr)
+		return;
+	stack->top = stack->top->next;
+	stack->size--;
+}
+void showStack(Stack*& stack)
+{
+	if (stack->top == nullptr)
+	{
+		cout << "It's empty stack :)" << endl;
+		return;
+	}
+	Node* curr = stack->top;
+	cout << "Top -> ";
+	for (int i = 1; i <= stack->size; i++)
+	{
+		cout << curr->data << " -> ";
+		curr = curr->next;
+	}
+	cout << "Bottom " << endl;
+}
+void insertElem(Stack*& stack, int pos, int data)
+{
+	Stack* tmp = new Stack;
+	tmp->init();
+	int init_size = stack->size; // size of element after it was initialized 
+	for (int i = 1; i <= init_size - pos + 1; i++)
+	{
+		Push(tmp, stack->top->data);
+		Pop(stack);
+	}
+	Push(stack, data);
+	init_size = tmp->size; // size of tmp stack after it was initialized 
+	for (int i = 1; i <= init_size; i++)
+	{
+		Push(stack, tmp->top->data);
+		Pop(tmp);
+	}
+	delete tmp;
+}
+void insertElems(Stack*& stack, int pos, int cnt)
+{
+	Stack* elems = new Stack;
+	elems->init();
+	int init_size = elems->size;
+	int data;
+	for (int i = 1; i <= cnt; i++)
+	{
+		cout << "Enter data of an element: ";
+		cin >> data;
+		Push(elems, data);
+	}
+	Stack* tmp = new Stack;
+	tmp->init();
+	init_size = stack->size;
+	for (int i = 1; i <= init_size - pos + 1; i++)
+	{
+		Push(tmp, stack->top->data);
+		Pop(stack);
+	}
+	init_size = elems->size;
+	for (int i = 1; i <= init_size; i++)
+	{
+		Push(stack, elems->top->data);
+		Pop(elems);
+	}
+	init_size = tmp->size; // size of tmp stack after it was initialized 
+	for (int i = 1; i <= init_size; i++)
+	{
+		Push(stack, tmp->top->data);
+		Pop(tmp);
+	}
+	delete tmp;
+	delete elems;
+}
+void removElem(Stack*& stack, int pos)
+{
+	Stack* tmp = new Stack;
+	tmp->init();
+	int init_size = stack->size;
+	for (int i = 1; i <= init_size - pos + 1; i++)
+	{
+		Push(tmp, stack->top->data);
+		Pop(stack);
+	}
+	Pop(tmp);
+	init_size = tmp->size;
+	for (int i = 1; i <= init_size; i++)
+	{
+		Push(stack, tmp->top->data);
+		Pop(tmp);
+	}
+	delete tmp;
+}
+void removElems(Stack*& stack, int pos, int cnt)
+{
+	Stack* tmp = new Stack;
+	tmp->init();
+	int init_size = stack->size; 
+	for (int i = 1; i < init_size - pos; i++)
+	{
+		Push(tmp, stack->top->data);
+		Pop(stack);
+	}
+	for (int i = 1; i <= cnt; i++)
+	{
+		Pop(stack);
+	}
+	init_size = tmp->size;
+	for (int i = 1; i <= init_size; i++)
+	{
+		Push(stack, tmp->top->data);
+		Pop(tmp);
+	}
+	delete tmp;
+}
+int main()
+{
+	Stack* stack = new Stack;
+	stack->init();
+	int size = 5, data, pos;
+	for (int i = 1; i < size; i++)
+	{
+		cout << "Enter a data of a new element: ";
+		cin >> data;
+		Push(stack, data);
+	}
+	showStack(stack);
+	int choice;
+	cout << "Choose the option:" << endl;
+	cout << "1 - push, 2 - pop, 3 - top, 4 - insertElem, 5 - insertElems, 6 - removElem, 7 - removElems" << endl;
+	cin >> choice;
+	//system("cls");
+	switch (choice)
+	{
+		case 1:
+		{
+			cout << "Enter data of the element:";
+			cin >> data;
+			Push(stack, data);
+			showStack(stack);
+			break;
+		}
+		case 2:
+		{
+			Pop(stack);
+			showStack(stack);
+			break;
+		}
+		case 3:
+		{
+			cout << "Top data = " << stack->top->data << endl;
+			showStack(stack);
+			break;
+		}
+		case 4:
+		{
+			cout << "Enter position and data of the element, you want to add: ";
+			cin >> pos >> data;
+			insertElem(stack, pos, data);
+			showStack(stack);
+			break;
+		}
+		case 5:
+		{
+			int cnt;
+			cout << "Enter position from which element will be inserted and quantity of them: ";
+			cin >> pos >> cnt;
+			insertElems(stack, pos, cnt);
+			showStack(stack);
+			break;
+		}
+		case 6:
+		{
+			cout << "Enter the position of element you want to remove: ";
+			cin >> pos;
+			removElem(stack, pos);
+			showStack(stack);
+			break;
+		}
+		case 7:
+		{
+			int cnt;
+			cout << "Enter position from which element will be removed and quantity of them: ";
+			cin >> pos >> cnt;
+			removElems(stack, pos, cnt);
+			showStack(stack);
+			break;
+		}
+		default:
+			cout << "Oops, something has gone wrong!" << endl;
+			break;
+	}
+	
+	return 0;
+}
