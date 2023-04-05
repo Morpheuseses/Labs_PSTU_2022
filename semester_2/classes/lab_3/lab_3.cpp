9594874694fe4c57c507dd57668d1e2593285c5d
Matvey <115172758+Morpheuseses@users.noreply.github.com>
2023-04-05 13:40:48 +0500

    Update lab_3.cpp

--- semester_2/classes/lab_3/lab_3.cpp
+++ semester_2/classes/lab_3/lab_3.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-using namespace std;
+using namespace std; 
 
 
 class Pair
@@ -11,7 +11,7 @@ public:
 	Pair()
 	{
 		first = 0;
-		second = 0;
+		second = 0; 
 	}
 	Pair(int f, double s)
 	{
@@ -23,7 +23,7 @@ public:
 		first = p.first;
 		second = p.second;
 	}
-	~Pair() {}
+	~Pair(){}
 	void setFirst(int first)
 	{
 		this->first = first;
@@ -40,56 +40,53 @@ public:
 	{
 		return second;
 	}
-	Pair& operator= (const Pair& p)
+	Pair operator= (const Pair& p)
 	{
 		this->first = p.first;
 		this->second = p.second;
-		return *this;
 	}
 	Pair& operator- (const Pair& p)
 	{
-		first -= p.first;
-		second -= p.second;
-		return *this;
+		first -= p.getFirst();
+		second -= p.getSecond();
+		return this;
 	}
 	Pair& operator+ (int x)
 	{
-		first += x;
+		first +=x;
 		return *this;
 	}
 	Pair& operator+ (double x)
 	{
-		second += x;
+		second +=x;
 		return *this;
 	}
-	friend ostream& operator<< (ostream& out, const Pair& p)
-	{
-		return (out << p.first << " : " << p.second << endl);
-	}
-	friend istream& operator>> (istream& in, const Pair& p)
-	{
-		int first; double second;
-		cout << "first?"; in >> first;
-		cout << "second?"; in >> second;
-		return in;
-	}
 };
-
+ostream& operator<< (ostream& out, const Pair& p)
+{
+	return (out << p.getFirst() << " : "<<  p.getSecond() << endl;)
+}
+istream& operator>> (istream& in, const Pair& p)
+{
+	int first; double second;
+	cout << "first?"; in >> first;
+	cout << "second?"; in >> second;
+	return in; 
+}
 
 int main()
 {
 	Pair p;
-	Pair p1(4, 9.0);
+	Pair p1(4,9.0);
 	Pair p2(p);
-	cout << p << endl;
-	cout << p1 << endl;
-	cout << p2 << endl;
+	cout << "There is first constructor(without parameters)\n" << p << endl;
+	cout << "There is second constroctor p1(4,9.0)\n" << p1 << endl;
+	cout << "There is copy constructor p2(p)\n" << p2 << endl;
 	p = p1;
-	cout << p << endl;
-	p = p1 - p2;
-	cout << p << endl;
-
+	cout << "This is how '=' operation works\n" << p1 << endl;
+	cout << "This is how output works\n " << p << endl;
 	cin >> p;
-	cout << p << endl;
-
+	cout << "This is how input works\n" << p << endl;
+	cout << "This is how addition works\n" << p + 1 << '\n' << p + 1.5 << endl;
+	cout << "This is how subtraction works\n" << p1 - 2 << endl;
 }
